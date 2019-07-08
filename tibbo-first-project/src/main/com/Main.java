ef7fafbad7f8786edd7d82450dd96e804069ddc9
Nikita Simonov <nikita.simonov@tibbo.com>
2019-07-08 16:22:08 +0300

    [add] first_work

--- tibbo-first-project/src/main/com/Main.java
+++ tibbo-first-project/src/main/com/Main.java
@@ -1,7 +1,29 @@
+
 public class Main
 {
   public static void main(String[] args)
   {
     System.out.println("Hello world!");
+  }
+  
+  public Integer plus(int[] array)
+  {
+    return null;
+  }
+  
+  public String encode(byte[] byteArray)
+  {
+    return null;
+  }
+  
+  public Boolean stringContains(String value, String subString)
+  {
+    return null;
+  }
+  
+  @Override
+  public String toString()
+  {
+    return null;
   }
 }
--- tibbo-first-project/src/test/com/MainTest.java
+++ tibbo-first-project/src/test/com/MainTest.java
@@ -1,11 +1,42 @@
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class MainTest extends TestCase
 {
+  @Test
   public void testMainClass() throws Exception
   {
     Main main = new Main();
     assertNotNull(main);
+  }
+  
+  @Test
+  public void testPlus() throws Exception
+  {
+    Main main = new Main();
+    int result = main.plus(MainTestHelper.INT_ARRAY);
+    assertEquals(145962204, result);
+  }
+  
+  @Test
+  public void testEncoding()
+  {
+    Main main = new Main();
+    assertEquals(MainTestHelper.STRING_STRING, main.encode(MainTestHelper.STRING_ARRAY));
+  }
+  
+  public void testMainName()
+  {
+    Main main = new Main();
+    assertEquals(MainTestHelper.MAIN_NAME, main.toString());
+  }
+  
+  @Test
+  public void testContains()
+  {
+    Main main = new Main();
+    assertFalse(main.stringContains(main.encode(MainTestHelper.STRING_ARRAY), "abraCadabra"));
+    assertTrue(main.stringContains(main.encode(MainTestHelper.STRING_ARRAY), "super"));
   }
   
   @Override
