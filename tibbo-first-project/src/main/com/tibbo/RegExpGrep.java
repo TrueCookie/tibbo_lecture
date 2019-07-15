120086997867e5a215b548789e50e7cbdd672400
TrueCookie <artemogloblin56@gmail.com>
2019-07-15 10:51:21 +0300

    with matcher

--- tibbo-first-project/src/main/com/tibbo/RegExpGrep.java
+++ tibbo-first-project/src/main/com/tibbo/RegExpGrep.java
@@ -6,12 +6,10 @@ import tibbo.grep.GrepException;
 
 public class RegExpGrep extends MainGrep {
     Pattern pattern = null;
-    Matcher matcher = null;
     public RegExpGrep(String value) throws GrepException {
         super(value);
         try {
             pattern = Pattern.compile(getTargetStr(), Pattern.CASE_INSENSITIVE);
-            matcher = pattern.matcher(value);
         } catch (PatternSyntaxException ex) {
             System.out.println(ex);
         }
@@ -21,6 +19,7 @@ public class RegExpGrep extends MainGrep {
     @Override
     //проверка входит ли подстрока в сторку
     public void checkValue(String value) {
+    Matcher matcher = pattern.matcher(value);
         if (matcher.find()) {
             getResultList().add(value);
         }
