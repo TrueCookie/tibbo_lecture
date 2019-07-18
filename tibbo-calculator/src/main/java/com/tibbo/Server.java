5a7b38b4f7e2e88c337a49a58a451f9babb4a1b2
Nikita Simonov <nikita.simonov@tibbo.com>
2019-07-18 10:30:40 +0300

    calculator_server_init

---
+++ tibbo-calculator/src/main/java/com/tibbo/Server.java
@@ -0,0 +1,15 @@
+package com.tibbo;
+
+public class Server
+{
+  private static final Server INSTANCE = new Server();
+  
+  public static void main(String[] args) throws Exception
+  {
+    INSTANCE.launch(args);
+  }
+  
+  private void launch(String[] args) throws Exception
+  {
+  }
+}
