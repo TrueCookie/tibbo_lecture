d449588cd31a10d44a75397ca78588f2a738b286
TrueCookie <artemogloblin56@gmail.com>
2019-07-19 11:13:36 +0300

    before sc upd

--- tibbo-calculator/src/main/java/com/tibbo/Server.java
+++ tibbo-calculator/src/main/java/com/tibbo/Server.java
@@ -3,20 +3,17 @@ package com.tibbo;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 
-public class Server
-{
-  private static final Server INSTANCE = new Server();
-  
-  public static void main(String[] args) throws Exception
-  {
-    INSTANCE.launch(args);
-  }
-  
-  private void launch(String[] args) throws Exception
-  {
-    ServerSocket serverSocket = new ServerSocket();
-    serverSocket.bind(new InetSocketAddress(15555));
-    serverSocket.setSoTimeout(5000);
-    serverSocket.close();
-  }
+public class Server {
+    private static final Server INSTANCE = new Server();
+
+    public static void main(String[] args) throws Exception {
+        INSTANCE.launch(args);
+    }
+
+    private void launch(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress(15555));
+        serverSocket.setSoTimeout(5000);
+        serverSocket.close();
+    }//зациклить accept, когда получен accept создать поток, зациклить поток и перейти обратно на получение accept
 }
--- tibbo-calculator/src/main/java/com/tibbo/database/DatabaseDAO.java
+++ tibbo-calculator/src/main/java/com/tibbo/database/DatabaseDAO.java
@@ -4,34 +4,62 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class DatabaseDAO
-{
-  
-  private static Session session = null;
-  
-  static
-  {
-    SessionFactory sessionFactory = new Configuration()
-        .configure("hibernate.cfg.xml")
-        .buildSessionFactory();
-    
-    session = sessionFactory.openSession();
-  }
-  
-  public static Session getSession()
-  {
-    return session;
-  }
-  
-  public static void registerUser(String username, String password)
-  {
-    //Выполнить Insert в базу данных
-  }
-  
-  public static boolean checkUserExist(String username, String password)
-  {
-    //Выполнить SELECT в базу данных
-    //если то, что вернулось не пусто, то true в другом случае false;
-    return true;
-  }
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseDAO {
+    private static Session session = null;
+    //private static final String url = "jdbc:postgresql://localhost:5432/server_calculator";
+
+    static {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+        session = sessionFactory.openSession();
+    }
+
+    public static Session getSession() {
+        return session;
+    }
+
+    public static void registerUser(String username, String password) {
+        //Выполнить Insert в базу данных
+        session.createQuery("INSERT INTO server_calculator VALUES (" + username + "," + password + ")");
+        /*try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO server_calculator VALUES (" + username + "," + password + ")");
+        } catch (Exception ex) {
+            System.out.printf(ex.toString());
+        }*/
+    }
+
+    public static boolean checkUserExist(String username, String password) {
+        //Выполнить SELECT в базу данных
+        //если то, что вернулось не пусто, то true в другом случае false;
+        boolean matchIsFounded = false;
+        List<ResultSet> resultList = session.createQuery("SELECT * FROM server_calculator").getResultList();
+        try {
+            for (ResultSet cell : resultList) {
+                if (cell.getString(username) == username && cell.getString(password) == password) {
+                    matchIsFounded = true;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.printf(ex.toString());
+        }
+        /*try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM server_calculator");
+            while (resultSet.next()) {
+                if(resultSet.getString(username) == username && resultSet.getString(password) == password){
+                    matchIsFounded = true;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.printf(ex.toString());
+        }*/
+        return matchIsFounded;
+    }
 }
