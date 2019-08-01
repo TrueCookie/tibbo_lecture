56bee2c02f64395e20a54fc68ad7360cda655f7b
Nikita Simonov <nikita.simonov@tibbo.com>
2019-08-01 15:25:31 +0300

    [add] datatables test //update innerTableTest

--- tibbo-calculator/src/test/java/com/tibbo/datatable/TestDataTable.java
+++ tibbo-calculator/src/test/java/com/tibbo/datatable/TestDataTable.java
@@ -49,7 +49,7 @@ public class TestDataTable extends TestCase
     
     DataRecord record = result.getRecord(0);
     assertNotNull(StaticDataTable.getSimpleTable());
-    assertEquals(record.getFormat(), StaticDataTable.getSimpleTable().getFormat());
+    assertEquals(record.getDataTable(StaticDataTableHelper.FIELD_DATATABLE_FIELD).getFormat(), StaticDataTable.getSimpleTable().getFormat());
   }
   
   public void testBigTable() throws Exception
